21aab991f4b5b26bc47893b1ed5c8feb45916e23
OmarNizam <eng.omarnizam@gmail.com>
2018-09-11 15:33:59 +0200

    Styles binding ex

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -10,12 +10,18 @@ export class TestComponent implements OnInit {
   public name = "Omar Nizam";
   public successClass = "text-success";
   public hasError = false;
+  public highlightcolor = "orange";
 
-  public isSpecial = true;
-  public messageClasses = {
-   "text-success": !this.hasError,
-   "text-danger": this.hasError,
-   "text-special": this.isSpecial
+ //  public isSpecial = true;
+ //  public messageClasses = {
+ //   "text-success": !this.hasError,
+ //   "text-danger": this.hasError,
+ //   "text-special": this.isSpecial
+ // };
+
+ public titleStyles = {
+   color: "blue",
+   fontStyle: "italic"
  };
 
   constructor() { }
